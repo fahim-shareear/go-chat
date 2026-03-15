a4cd30abd6bfa72e4d799ea5ba14f6e8ffcaf4e1
fahim-shareear <fahimshareear@gmail.com>
2026-03-16 00:20:19 +0600

    rolling back to old code

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,33 +4,30 @@ import authRoutes from "./router/auth.router.js";
 import messageRoutes from "./router/message.router.js";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-// ESM compatible __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Middleware
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-// Frontend path
-const frontendDistPath = path.join(__dirname, "../../frontend/dist");
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-// Serve React static files
-app.use(express.static(frontendDistPath));
+  app.use(express.static(frontendPath));
 
-// Catch-all for SPA
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendDistPath, "index.html"));
-});
+  // SPA fallback
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
-// Start server
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server is running on port ${port}`)
+);
