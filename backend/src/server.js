5f5226e3dff996d508537ecebd76b77fba6cbc7d
fahim-shareear <fahimshareear@gmail.com>
2026-03-15 22:54:10 +0600

    fixing missing path and production code

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,24 +5,29 @@ import messageRoutes from "./router/message.router.js";
 import dotenv from "dotenv";
 import path from "path";
 
-app.use(cors());
-app.use(express.json());
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-//make ready for production:
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
 
-    app.get("*", (req, res) =>{
-        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
-    })
+  app.use(express.static(frontendPath));
+
+  // SPA fallback
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
-app.listen(port, ()=> console.log(`Server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server is running on port ${port}`)
+);
