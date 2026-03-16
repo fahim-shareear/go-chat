83ec9ff7ecdc72760319a23e6db49e8ea5511b04
fahim-shareear <fahimshareear@gmail.com>
2026-03-16 12:58:04 +0600

    sign up controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,35 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./router/auth.router.js";
+import messageRoutes from "./router/message.router.js";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// important path
-const frontendDistPath = path.join(__dirname, "../../../frontend/dist");
+const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(frontendDistPath));
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(frontendDistPath, "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  app.use(express.static(frontendPath));
+
+  // SPA fallback
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+    connectDB()
 });
